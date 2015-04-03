caaf9edd0d50edaf52246d18a83e94981edcbb84
priyanka.goel <priyanka@grabhouse.com>
2015-04-03 17:42:57 +0530

    basic validation added in building and flat data

--- src/forms/FlatData.java
+++ src/forms/FlatData.java
@@ -9,6 +9,143 @@ import java.util.List;
 public class FlatData {
 
 	public List<SectionListOfFileNamePair> picturelist;
+	public List<SectionListOfFileNamePair> getPicturelist() {
+		return picturelist;
+	}
+
+	public void setPicturelist(List<SectionListOfFileNamePair> picturelist) {
+		this.picturelist = picturelist;
+	}
+
+	public String getFlatnumber() {
+		return flatnumber;
+	}
+
+	public void setFlatnumber(String flatnumber) {
+		this.flatnumber = flatnumber;
+	}
+
+	public Integer getFloornumber() {
+		return floornumber;
+	}
+
+	public void setFloornumber(Integer floornumber) {
+		this.floornumber = floornumber;
+	}
+
+	public Integer getNumOfBathrooms() {
+		return numOfBathrooms;
+	}
+
+	public void setNumOfBathrooms(Integer numOfBathrooms) {
+		this.numOfBathrooms = numOfBathrooms;
+	}
+
+	public Integer getNumOfBalconys() {
+		return numOfBalconys;
+	}
+
+	public void setNumOfBalconys(Integer numOfBalconys) {
+		this.numOfBalconys = numOfBalconys;
+	}
+
+	public String getFurnishingtype() {
+		return furnishingtype;
+	}
+
+	public void setFurnishingtype(String furnishingtype) {
+		this.furnishingtype = furnishingtype;
+	}
+
+	public String getFlattype() {
+		return flattype;
+	}
+
+	public void setFlattype(String flattype) {
+		this.flattype = flattype;
+	}
+
+	public String getFacing() {
+		return facing;
+	}
+
+	public void setFacing(String facing) {
+		this.facing = facing;
+	}
+
+	public Integer getRent() {
+		return rent;
+	}
+
+	public void setRent(Integer rent) {
+		this.rent = rent;
+	}
+
+	public Integer getMaintainancecharges() {
+		return maintainancecharges;
+	}
+
+	public void setMaintainancecharges(Integer maintainancecharges) {
+		this.maintainancecharges = maintainancecharges;
+	}
+
+	public Integer getDeposit() {
+		return deposit;
+	}
+
+	public void setDeposit(Integer deposit) {
+		this.deposit = deposit;
+	}
+
+	public Boolean getNegotiable() {
+		return negotiable;
+	}
+
+	public void setNegotiable(Boolean negotiable) {
+		this.negotiable = negotiable;
+	}
+
+	public List<String> getAvailableFor() {
+		return availableFor;
+	}
+
+	public void setAvailableFor(List<String> availableFor) {
+		this.availableFor = availableFor;
+	}
+
+	public RatingAndListOfEs<String> getAmenities() {
+		return amenities;
+	}
+
+	public void setAmenities(RatingAndListOfEs<String> amenities) {
+		this.amenities = amenities;
+	}
+
+	public String getServicesyouwillbeinterestedin() {
+		return servicesyouwillbeinterestedin;
+	}
+
+	public void setServicesyouwillbeinterestedin(
+			String servicesyouwillbeinterestedin) {
+		this.servicesyouwillbeinterestedin = servicesyouwillbeinterestedin;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	String flatnumber;
 	Integer floornumber;
 	Integer numOfBathrooms;
--- src/validators/BuildingAndFlatDataValidator.java
+++ src/validators/BuildingAndFlatDataValidator.java
@@ -1,13 +1,11 @@
 package validators;
 
 import helperclasses.ErrorFieldAndMessage;
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 import forms.BuildingAndFlatData;
-
+import forms.FlatData;
 
 public class BuildingAndFlatDataValidator {
 
@@ -16,6 +14,103 @@ public class BuildingAndFlatDataValidator {
 		// TODO Auto-generated method stub
 		List<ErrorFieldAndMessage> errorfieldandstringlist = new ArrayList<>();
 
+		if (buildingAndFlatData.getUsername() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("username",
+					"username can't be left blank"));
+		// if (pgAndTenantData.getGpslocation() == null)
+		// errorfieldandstringlist.add(new ErrorFieldAndMessage("gpslocation",
+		// "Please provide gps location"));
+		if (buildingAndFlatData.getDeviceId() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("DeviceId",
+					"Please provide DeviceId"));
+		if (buildingAndFlatData.getToken() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("token",
+					"Please provide token provided to you"));
+		if (buildingAndFlatData.getPropertyId() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("propertyId",
+					"Please provide PropertyId"));
+		if (buildingAndFlatData.getBuildingData().getPicturelist().size() == 0)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("picturelist",
+					"Please provide Picture list"));
+		// else {
+		// int photocount = 0;
+		// for (SectionListOfFileNamePair sectionListOfFileNamePair :
+		// pgAndTenantData
+		// .getPgdata().getPicturelist()) {
+		// photocount += sectionListOfFileNamePair.photonamelist.size();
+		//
+		// if (photocount < 5)
+		// errorfieldandstringlist
+		// .add(new ErrorFieldAndMessage("picturelist",
+		// "picture list should contain at least 5 photo names"));
+		// }
+		// }
+
+		if (buildingAndFlatData.getBuildingData().getSocietyname() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("societyname",
+					"Please provide Societyname"));
+		if (buildingAndFlatData.getBuildingData().getSocietyname().length() < 2
+				|| buildingAndFlatData.getBuildingData().getSocietyname()
+						.length() > 50)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("societyname",
+					"societyname name cant be less than 2 or more than 50 characters"));
+		if (buildingAndFlatData.getBuildingData().getBuildingname().length() > 50)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage(
+					"buildingname",
+					"buildingname name cant be more than 50 characters"));
+		if (buildingAndFlatData.getBuildingData().getAddressl1() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("addressl1",
+					"Address Line 1 can't be left blank"));
+		if (buildingAndFlatData.getBuildingData().getAddressl1().length() > 100)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("addressl1",
+					"addressl1 name cant be more than 100 characters"));
+		if (buildingAndFlatData.getBuildingData().getAddressl2() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("addressl2",
+					"Address Line 2 can't be left blank"));
+		if (buildingAndFlatData.getBuildingData().getAddressl2().length() > 100)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("addressl2",
+					"addressl2 name cant be more than 100 characters"));
+		if (buildingAndFlatData.getBuildingData().getPincode() == 0)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("pincode",
+					"Pincode can't be left blank"));
+		if (buildingAndFlatData.getBuildingData().getPincode() > 999999)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("pincode",
+					"Pincode can't be more than 6 digits"));
+		if (buildingAndFlatData.getBuildingData().getLandmark().length() > 100)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage("landmark",
+					"landmark cant be more than 100 characters"));
+		if (buildingAndFlatData.getBuildingData().getAvailableFor().size() == 0)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage(
+					"availableFor", "availableFor can't be left blank"));
+
+		// if (pgAndTenantData.getPgdata().getAgeband().getminlimit() >
+		// pgAndTenantData
+		// .getPgdata().getAgeband().getmaxlimit())
+		// errorfieldandstringlist.add(new ErrorFieldAndMessage("ageBand",
+		// "min age limit should be less than age max limit"));
+		if (buildingAndFlatData.getBuildingData().getTypesandavailability()
+				.getAlldetails().size() > 7)
+			errorfieldandstringlist
+					.add(new ErrorFieldAndMessage("typesandavailability",
+							"typesandavailability should not contain more than 7 items"));
+
+		if (buildingAndFlatData.getBuildingData().getSelectedlocation() == null)
+			errorfieldandstringlist.add(new ErrorFieldAndMessage(
+					"selectedlocation", "Please select a location fro pg"));
+		// if ((Math.abs(pgAndTenantData.getgpslocation().getLat()
+		// - pgAndTenantData.getPgdata().getselectedlocation().getLat()) > 0.01)
+		// || (Math.abs(pgAndTenantData.getgpslocation().getLongi()
+		// - pgAndTenantData.getPgdata().getselectedlocation()
+		// .getLongi()) > 0.01))
+		// errorfieldandstringlist.add(new ErrorFieldAndMessage("gpsLocation",
+		// "please select correct location of yours"));
+
+		for (FlatData flatdata : buildingAndFlatData.getFlatData()) {
+			if (flatdata.getFlatnumber() == null)
+				errorfieldandstringlist.add(new ErrorFieldAndMessage(
+						"flatnumber", "flatnumber can't be left blank"));
+
+		}
 
 		return errorfieldandstringlist;
 	}
