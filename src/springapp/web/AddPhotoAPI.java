b2a8fad46ed33d69749581469f13b125e2982a93
Pavithra Kumar Shetty <pkshetty@gmail.com>
2015-04-10 15:15:00 +0000

    Implement cloudinary image upload add back springapp-servlet to resources - affects too many code changes

--- src/springapp/web/AddPhotoAPI.java
+++ src/springapp/web/AddPhotoAPI.java
@@ -1,10 +1,5 @@
 package springapp.web;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.Map;
 
 import models.PhotoModel;
 
@@ -20,72 +15,51 @@ import org.springframework.web.multipart.MultipartFile;
 
 import validators.PhotoAPIValidator;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
 @Controller
 public class AddPhotoAPI {
 	
-	private static Properties prop;
-	
-	@RequestMapping(value = "/findconfigproperty", method = RequestMethod.POST)
-	private @ResponseBody String getConfigProperty() throws IOException {
-		if(prop == null){
-			prop = new Properties();
-			InputStream input = AddPhotoAPI.class.getClassLoader().getResourceAsStream("config.properties");
-			if(input==null){
-				return "config.properties file not found";  // make it null wen u dont want to use dis method anymore ..
-			}
-			prop.load(input);
-			input.close();
-		}
-		return prop.getProperty("filepath");
-	}
-	
 	@RequestMapping(value = "/uploadphoto", method = RequestMethod.POST)
-	public @ResponseBody String addphoto(@RequestParam String propertyId, @RequestParam String flatnumber,
-			@RequestParam String propertyType, @RequestParam String section,
-			@RequestParam String photoname,
+	public @ResponseBody String addphoto(@RequestParam("propertyId") String propertyId, @RequestParam("flatnumber") String flatnumber,
+			@RequestParam("propertyType") String propertyType, @RequestParam("section") String section,
+			@RequestParam("photoname") String photoname,
 			@RequestParam("file") MultipartFile file) throws Exception {
 
 		ApplicationContext ctx = new GenericXmlApplicationContext(
 				"springapp-servlet.xml");
 		MongoOperations mongoOperation = (MongoOperations) ctx
 				.getBean("mongoTemplate");
-		
 		String photoAPIValidatorresult= PhotoAPIValidator.validate(propertyId, flatnumber, propertyType, section, photoname);
 		if(!photoAPIValidatorresult.equals("Successful"))
 			return photoAPIValidatorresult;
-
-		// Create directory
-		if(getConfigProperty()==null) return "config.properties file not found";
-		File files = new File(getConfigProperty()
-				+ propertyId + "/" + section);
-		if (!files.exists()) {
-			if (files.mkdirs()) {
-				System.out.println("Multiple directories are created!");
-			} else {
-				System.out.println("Failed to create multiple directories!");
-			}
-		}
-
-		String uploadedFileLocation = getConfigProperty()
-				+ propertyId + "/" + section + "/" + photoname;
-// updated file if file already exists
-		if (!file.isEmpty()) {
+		try {
+			// TODO : Make this configurable. Separate accounts for production and dev - shetty 10-apr-15
+			
+			// THIS IS FOR DEVELOPEMNT
+			/*Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+					  "cloud_name", "grbh",
+					  "api_key", "266747318345815",
+					  "api_secret", "NID3oheFJGcg1Sisu2hTgePpiv0"));*/
+			// PRODUCTION SETTINGS
+			Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+					  "cloud_name", "grabhouse",
+					  "api_key", "573923864849251",
+					  "api_secret", "U7q8f_oV0proD8NDPu3evypgV6M"));
+			
+			Map params = ObjectUtils.asMap("folder", "/dc/" + propertyId);
+			Map uploadResult = cloudinary.uploader().upload(file.getBytes(), params);
 			try {
-				byte[] bytes = file.getBytes();
-				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(new File(uploadedFileLocation)));
-				stream.write(bytes);
-				stream.close();
 				mongoOperation.save(new PhotoModel(propertyId, propertyType,
-						section, photoname, uploadedFileLocation));
-				return "You successfully uploaded " + "!";
+						section, photoname, (String) uploadResult.get("url")));
+				return "File uploaded successfully!";
 			} catch (Exception e) {
-				return "You failed to upload  => " + e.getMessage();
+				e.printStackTrace();
+				return "Failed to upload the file "+e.getMessage();
 			}
-		} else {
-			return "You failed to upload because the file was empty.";
+		} catch (RuntimeException  e) {
+			return "Failed to upload the file : "+e.getMessage();
 		}
-		
-
 	}
 }
